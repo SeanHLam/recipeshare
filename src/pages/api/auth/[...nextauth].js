fe93bb7b66842be73d460bbe85c603319b566228
SeanHLam <91505213+SeanHLam@users.noreply.github.com>
2023-03-31 21:03:27 -0700

    edited env

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -9,8 +9,8 @@ export const authOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
-      clientId: process.env.GITHUB_ID,
-      clientSecret: process.env.GITHUB_SECRET,
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
  
     // ...add more providers here
@@ -18,7 +18,8 @@ export const authOptions = {
   callbacks: {
     async signIn(user, account, profile) {
       return '/home';
-    }
+    },
+  
   },
 
 }
